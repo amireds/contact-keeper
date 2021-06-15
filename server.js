64532e50365140fed8d40c4bb515359aec833fcb
amireds <zoe_steve@hotmail.com>
2021-06-15 22:45:09 +0100

    Data validation with express validator Password Encryption with Bcrypt Generation login token with JWT - Json Web Token

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
-const express = require('express');
-const connectDb = require('./config/db')
+const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
 // Connect Database
-connectDb()
+connectDB();
 
-app.get('/', (req, res) => res.json({msg: 'Welcome to the contact keeper API...'}))
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) =>
+  res.json({ msg: "Welcome to the contact keeper API..." })
+);
 
 // Define Routes
-app.use('/api/users', require('./routes/users'));
-app.use('/api/contacts', require('./routes/contacts'));
-app.use('/api/auth', require('./routes/auth'));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/contacts", require("./routes/contacts"));
+app.use("/api/auth", require("./routes/auth"));
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-
