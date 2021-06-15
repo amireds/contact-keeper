1f0230abc25a17d8bf95a685d5c762a6330f1e90
amireds <zoe_steve@hotmail.com>
2021-06-16 00:01:53 +0100

    Authenticated the login with the generated Token

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,5 +2,19 @@ const jwt = require("jsonwebtoken");
 const config = require("config");
 
 module.exports = function (req, res, next) {
-  // Get
+  // Get token from the header
+  const token = req.header("x-auth-token");
+
+  // Check if not token
+  if (!token) {
+    return res.status(401).json({ msg: "No Token, authorization denied" });
+  }
+  try {
+    const decoded = jwt.verify(token, config.get("jwtSecret"));
+
+    req.user = decoded.user;
+    next();
+  } catch (e) {
+    res.status(401).json({ msg: "Token is not valid" });
+  }
 };
