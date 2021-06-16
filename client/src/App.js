1cbdadd37e5002060a8c1c3721172673353fd832
amireds <zoe_steve@hotmail.com>
2021-06-17 00:37:02 +0100

    Login component and authentication routes created

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import About from "./pages/About";
 import "./App.css";
 import AuthState from "./context/auth/AuthState";
 import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
                 <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/register" component={Register} />
+                <Route exact path="/login" component={Login} />
               </Switch>
             </div>
           </Fragment>
